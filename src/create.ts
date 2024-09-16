824e45fbac543fa24b70d67b29cc386effe242d5
Jason Silvers <jason.silvers@dispel.com>
2024-09-16 09:14:34 -0400

    remove websocket

--- src/create.ts
+++ src/create.ts
@@ -2,10 +2,9 @@ import { AnyTRPCRouter } from '@trpc/server'
 
 import { MswTrpc, TRPCMswConfig } from './types.js'
 
-import { HttpHandler } from 'msw'
 import { trpc } from './handler.js'
 
-const createTRPCMsw = <Router extends AnyTRPCRouter>(config: TRPCMswConfig) => {
+export const createTRPCMsw = <Router extends AnyTRPCRouter>(config: TRPCMswConfig) => {
   const { links, transformer } = config
 
   const createUntypedTRPCMsw = (pathParts: string[] = []) => {
@@ -18,15 +17,6 @@ const createTRPCMsw = <Router extends AnyTRPCRouter>(config: TRPCMswConfig) => {
             return (handler: Function) => {
               const result = trpc[lastKey](procedurePath, handler, { links, transformer })
 
-              if (result instanceof HttpHandler) {
-                return result
-              }
-
-              return result.handler
-            }
-          } else if (lastKey === 'subscription') {
-            return (handler?: Function) => {
-              const result = trpc[lastKey](procedurePath, handler, { links, transformer })
               return result
             }
           }
@@ -39,5 +29,3 @@ const createTRPCMsw = <Router extends AnyTRPCRouter>(config: TRPCMswConfig) => {
 
   return createUntypedTRPCMsw() as MswTrpc<Router>
 }
-
-export default createTRPCMsw
